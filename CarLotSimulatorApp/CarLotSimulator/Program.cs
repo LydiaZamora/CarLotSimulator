5069e41d2bffd42c1b670cd5b84ed67d0d4a8781
Lydia Zamora <lydiazamorayoga@gmail.com>
2024-06-30 13:54:23 -0700

    written steps to remember

--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -7,9 +7,15 @@ namespace CarLotSimulator
     {
         static void Main(string[] args)
         {
+            // Instantiated a new object using the CarLot class
+            // so I can now access properties etc.
             var lot = new CarLot();
 
+            //instantiated random car using the class Car - make new object
+
             var randomCar = new Car();
+
+            // adding properties to this new object
             randomCar.Year = 2018;
             randomCar.Make = "Ford";
             randomCar.Model = "Focus";
@@ -17,8 +23,12 @@ namespace CarLotSimulator
             randomCar.HonkNoise = "meep meep";
             randomCar.IsDriveable = true;
 
+            //called methods from car class
+
             randomCar.MakeEngineNoise();
             randomCar.MakeHonkNoise();
+
+            //use onject initialization syntax
 
             var newCar = new Car()
             {
@@ -33,14 +43,23 @@ namespace CarLotSimulator
             newCar.MakeEngineNoise();
             newCar.MakeHonkNoise();
 
+            //use method that calls in ALL arguments
+            //passed into constructor from car class
+
             var oldCar = new Car(1998, "Honda", "Civic", "vrooom", "meep", false);
 
             oldCar.MakeEngineNoise();
             oldCar.MakeHonkNoise();
 
+            //Added cars to carlot List named carDataBase
+
             lot.carDataBase.Add(randomCar);
             lot.carDataBase.Add(newCar);
             lot.carDataBase.Add(oldCar);
+
+
+            //used foreach loop to print the cars' tear make
+            //and model in the list carDataBase to console screend
 
             foreach(var car in lot.carDataBase)
             {
